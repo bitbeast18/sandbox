1aed1d0e417c8e722f2c22bae63ae81d61340af7
bitbeast18 <pro.bbcom18@gmail.com>
2020-03-09 00:43:44 +0530

    Added monaco plugin and  webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+const MonacoEditorPlugin = require('monaco-editor-webpack-plugin');
+
 module.exports = {
   transpileDependencies: ["vuetify"],
+
+  configureWebpack: {
+    plugins: [
+      new MonacoEditorPlugin({
+        languages: ['c', 'cpp', 'java', 'python', 'javascript', 'html', 'css'],
+        features: ['!gotoSymbol']
+      })
+    ]
+
+  },
+
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
